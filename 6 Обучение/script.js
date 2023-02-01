06fb1171c6534442f260389a033b47a558927de4
scythianmozg <121483742+scythianmozg@users.noreply.github.com>
2023-02-01 16:10:14 +0300

    курс окончен
    курс окончен

--- 6 Обучение/script.js
+++ 6 Обучение/script.js
@@ -1,10 +1,14 @@
 let phrases = [
-  'отправить другу смешную гифку',
-  'посмотреть скидки на авиабилеты',
-  'разобраться, о чём поют рэперы',
-  'Юрий Дудь',
-  'расставить книги на полке по цвету',
-  'читать про зарплаты в Сан-Франциско'
+  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+{ text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+{ text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+{ text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
+{ text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+{ text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+{ text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+{ text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+{ text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+{ text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
 ];
 
 function getRandomElement(arr) {
@@ -17,10 +21,15 @@ let phrase = document.querySelector('.phrase');
 let advice = document.querySelector('.advice');
 let image = document.querySelector('.image');
 
-phrase.textContent = getRandomElement(phrases);
-button.textContent = getRandomElement(phrases);
-
 button.addEventListener('click', function () {
-    console.log('Счастье не за горами');
-    button.textContent = getRandomElement(phrases);
-}); 
+  let randomElement = getRandomElement(phrases);  
+  smoothly(phrase, 'textContent', randomElement.text);
+  //image.src = randomElement.image; // вот так
+  smoothly(image, 'src', randomElement.image); 
+
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = '33px';
+  } else {
+    advice.style.fontSize = '42px';
+  }
+});
